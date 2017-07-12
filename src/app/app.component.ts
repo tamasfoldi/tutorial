20eb828d04485d226865c53cd33c5fb6d3396c15
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-07-12 20:00:18 +0930

    provide factory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from './services/title.service';
+import { APP_TITLE } from './tokens';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: TitleService,
+      useFactory: (title: string) => new TitleService(`${title}_factory`),
+      deps: [APP_TITLE]
+    }
+  ]
 })
 export class AppComponent implements OnInit {
   title: string;

91fc5861d07ec54161ccadda91009d8632f1375e
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-07-12 21:13:43 +0930

    no leak

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-child',
@@ -7,18 +7,25 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./child.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnDestroy {
   counter = 0;
   @Input('counterStream')
   counterStream: Observable<number>;
 
+  componentDestroy = new Subject();
+
   constructor(private _chDR: ChangeDetectorRef) { }
 
   ngOnInit() {
-    this.counterStream.subscribe(i => { // leaks
+    this.counterStream.takeUntil(this.componentDestroy).subscribe(i => {
       this.counter += i;
       this._chDR.markForCheck();
     })
+  }
+
+  ngOnDestroy() {
+    this.componentDestroy.next();
+    this.componentDestroy.unsubscribe();
   }
 
 }

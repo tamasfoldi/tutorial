fbd4018a9dbaf4faed469f5166dec884c0b0094e
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-07-13 18:28:54 +0930

    reactive forms

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -11,13 +12,19 @@ export class FormComponent implements OnInit {
     email: '@',
     number: -1
   }
-  constructor() { }
+
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      text: [this.model.text, Validators.minLength(3)],
+      email: [this.model.email, Validators.email],
+      number: [this.model.text, Validators.required]
+    });
   }
 
   onSubmit() {
-    console.log('sadf');
   }
 
 }

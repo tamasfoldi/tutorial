7db773315ea955673fca4abaaad0c3954d351261
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-07-13 18:28:54 +0930

    reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
@@ -12,7 +12,7 @@ import { FormComponent } from './components/form/form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

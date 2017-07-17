0aaeeaa92ff184d5c73b6cf3f4066d02bfa75d96
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-07-17 19:50:52 +0930

    [add] test

--- src/app/services/http-client.service.spec.ts
+++ src/app/services/http-client.service.spec.ts
@@ -1,15 +1,28 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClientService } from './http-client.service';
 
 describe('HttpClientService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [HttpClientService]
     });
   });
 
-  it('should be created', inject([HttpClientService], (service: HttpClientService) => {
-    expect(service).toBeTruthy();
+  afterEach(inject([HttpTestingController], (mockHttp: HttpTestingController) => {
+    mockHttp.verify();
   }));
+
+  it('expects a GET request', inject([HttpClient, HttpTestingController, HttpClientService],
+    (http: HttpClient, httpMock: HttpTestingController, httpClientService: HttpClientService) => {
+      httpClientService.getData()
+        .subscribe(data => expect(data['name']).toEqual('Test Data'));
+
+      const req = httpMock.expectOne('http://localhost:8080/Kartyarendszer/api/CardRequest');
+      expect(req.request.method).toEqual('GET');
+
+      req.flush({ name: 'Test Data' });
+    }));
 });
